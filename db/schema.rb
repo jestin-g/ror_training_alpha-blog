867c3897697489dd34a68843653a06f42b24e2e6
Gabriel Jestin <jestingabriel@gmail.com>
2023-04-26 15:27:01 +0200

    Added migration file add_timestamps_articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_26_131446) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_26_132442) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

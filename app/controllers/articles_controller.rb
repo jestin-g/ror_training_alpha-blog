0db37859e4d903ce8a892bf73bbc741db973f992
Gabriel Jestin <jestingabriel@gmail.com>
2023-04-26 16:55:27 +0200

    Added articles listing page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
   def show
-    binding.break
     @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
   end
 end
